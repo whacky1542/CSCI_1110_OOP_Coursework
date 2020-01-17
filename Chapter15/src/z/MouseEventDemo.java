26d062c0efc28280ee655cfd4fc51928e21565c7
404kevnotfound <student@HSiMac4-7.local>
2020-01-17 10:59:44 -0700

    Add more notes

---
+++ Chapter15/src/z/MouseEventDemo.java
@@ -0,0 +1,32 @@
+package z;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MouseEventDemo extends Application {
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		// Create a pane and set its properties
+		Pane pane = new Pane();
+		Text text = new Text(20, 20, "Programming is fun");
+		pane.getChildren().addAll(text);
+		text.setOnMouseDragged(e -> {
+			text.setX(e.getX());
+			text.setY(e.getY());
+		});
+
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane, 300, 100);
+		primaryStage.setTitle("MouseEventDemo"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Application Launch");
+		Application.launch(args);
+	}
+}
---
+++ Chapter15/src/z/ObservablePropertyDemo.java
@@ -0,0 +1,26 @@
+package z;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class ObservablePropertyDemo {
+	public static void main(String[] args) {
+		DoubleProperty balance = new SimpleDoubleProperty();
+		/*
+		balance.addListener(new InvalidationListener() {
+			public void invalidated(Observable ov) {
+				System.out.println("The new value is " + balance.doubleValue());
+			}
+		});
+		*/
+		
+		// Simplified version of the above:
+		balance.addListener(ov -> {
+			System.out.println("The new value is " + balance.doubleValue());
+		});
+
+		balance.set(4.5);
+	}
+}
