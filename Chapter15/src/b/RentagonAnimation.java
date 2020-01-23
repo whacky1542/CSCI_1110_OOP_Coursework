78c90e1e28bc06b093f337f97f18a592eba835e3
404kevnotfound <student@HSiMac4-7.local>
2020-01-23 08:56:32 -0700

    Finished chapter 15

--- Chapter15/src/b/RentagonAnimation.java
+++ Chapter15/src/b/RentagonAnimation.java
@@ -1,10 +1,16 @@
 package b;
 
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
 import javafx.animation.Animation.Status;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -15,42 +21,58 @@ import javafx.util.Duration;
 
 public class RentagonAnimation extends Application {
 	public void start(Stage primaryStage) {
-		Rectangle rectangle = new Rectangle(0, 0, 40, 10);
+		// Rectangle
+		Rectangle rectangle = new Rectangle(40, 20);
 		rectangle.setFill(Color.RED);
 		rectangle.setStroke(Color.BLACK);
-		
+				
+		// Pentagon
 		Polygon pentagon = new Polygon();
 		ObservableList<Double> list = pentagon.getPoints();
 		
 		for (int i = 0; i < 5; i++) {
-			list.add(100 * Math.cos(2 * i * Math.PI / 5)); 
-			list.add(100 * Math.sin(2 * i * Math.PI / 5));
+			list.add(200 * Math.cos(2 * i * Math.PI / 5)); 
+			list.add(200 * Math.sin(2 * i * Math.PI / 5));
 		}
 		
 		pentagon.setRotate(-18);
 		pentagon.setFill(Color.TRANSPARENT);
 		pentagon.setStroke(Color.BLACK);
+		pentagon.setLayoutY(10);
 		
-		PathTransition pt = new PathTransition();
-		pt.setDuration(Duration.millis(5000));
-		pt.setPath(pentagon);
-		pt.setNode(rectangle);
+		// Path Transition
+		PathTransition pt = new PathTransition(Duration.millis(5000), pentagon, rectangle);
 		pt.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
 		pt.setCycleCount(Timeline.INDEFINITE);
-		pt.setInterpolator();
+		pt.setInterpolator(Interpolator.LINEAR);
 		pt.play();
 		
+		// Fade Transition
+		FadeTransition ft = new FadeTransition(Duration.millis(2500), rectangle);
+		ft.setFromValue(1.0);
+		ft.setToValue(0);
+		ft.setCycleCount(Timeline.INDEFINITE);
+		ft.setAutoReverse(true);
+		ft.play();
+		
+		// Stack Pane
 		StackPane pane = new StackPane();
 		pane.getChildren().addAll(pentagon, rectangle);
 		
+		// Event Handling
 		pane.setOnMouseClicked(e -> {
-			if (pt.getStatus() == Status.PAUSED)
+			if (pt.getStatus() == Status.PAUSED) {
 				pt.play();
-			else
+				ft.play();
+			}
+			else {
 				pt.pause();
+				ft.pause();
+			}
 		});
 		
-		Scene scene = new Scene(pane, 400, 400);
+		// Setting the Stage
+		Scene scene = new Scene(pane, 500, 500);
 		primaryStage.setTitle("Rentagon Animation");
 		primaryStage.setScene(scene);
 		primaryStage.show();
